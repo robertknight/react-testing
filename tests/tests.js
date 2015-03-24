f508188ecddfe5f1216d6fc86b55b178bc3b0763
Robert Knight <robert.knight@mendeley.com>
2015-03-24 07:40:46 +0000

    Avoid using an expression for require()
    Use string literals to make requires visible to React

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,5 @@
 // this is the entry point for the test suite for use in
 // webpack
-const testModules = [
-	'./TweetItem_test',
-	'./TweetList_test'
-];
+require('./TweetItem_test');
+require('./TweetList_test');
 
-testModules.forEach(testModule => require(testModule));
